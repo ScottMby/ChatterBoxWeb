3f036914634f0a8a8e3c9a9478165fee4f7784cd
scottmumby <smumby2003@gmail.com>
2024-08-12 15:56:06 +0100

    Added Login and User Persistance

--- src/app/auth-service.ts
+++ src/app/auth-service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from "@angular/core";
-import { inject } from "@angular/core";
-import { Auth, createUserWithEmailAndPassword, updateProfile } from "@angular/fire/auth"
+import { Injectable, inject, signal } from "@angular/core";
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, User, user } from "@angular/fire/auth"
 import { from, Observable } from "rxjs";
+import { UserInterface } from "./user.interface";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    firebaseAuth = inject(Auth)
+    firebaseAuth = inject(Auth);
+    user$ = user(this.firebaseAuth);
+    currentUserSig = signal<UserInterface | null | undefined>(undefined); //If null user is not logged in. To get user details do currentUserSig?.<detail>
 
     register(email: string, username: string, password: string) : Observable<void>
     {
@@ -18,7 +20,16 @@ export class AuthService {
         ).then(response => 
             updateProfile(response.user, {displayName: username})
         );
-        
+        return from(promise);
+    }
+
+    login(email: string, password: string) : Observable<void>
+    {
+        const promise = signInWithEmailAndPassword(
+            this.firebaseAuth,
+            email,
+            password
+        ).then(() => {})
         return from(promise);
     }
 
